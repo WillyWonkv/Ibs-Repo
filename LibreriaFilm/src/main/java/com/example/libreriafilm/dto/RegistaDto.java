f3b237939e251cbc468bfd32bc1a0ebf2ff3a9b4
WillyWonkv <69716539+WillyWonkv@users.noreply.github.com>
2025-10-22 14:21:08 +0200

    1

--- LibreriaFilm/src/main/java/com/example/libreriafilm/dto/RegistaDto.java
+++ LibreriaFilm/src/main/java/com/example/libreriafilm/dto/RegistaDto.java
@@ -1,12 +1,10 @@
 package com.example.libreriafilm.dto;
 
-import com.example.libreriafilm.entity.Film;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import jakarta.validation.constraints.NotBlank;
 
 import java.util.Date;
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record RegistaDto(long id, @NotBlank String nome, Date dataNascita, List<FilmDto> film) {
+public record RegistaDto(long id, String nome, Date dataNascita, List<FilmDto> film) {
 }
